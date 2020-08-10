f5a8f1a9552ad2fb557482ee7005d5b3913915e0
harniy <forbedlam@gmail.com>
2020-08-10 13:49:10 +0300

    first commit

---
+++ movies/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.forms import ModelForm
+from .models import Profile, Comment
+
+
+class CustomerForm(ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('avatar',)
+        exclude = ['user']
+
+
+class CommentForm(ModelForm):
+    content = forms.CharField(label="", widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder' : 'комментарий...', 'rows': '4', 'cols':'50'}))
+    class Meta:
+        model = Comment
+        fields = ('content',)
---
+++ movies/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.MoviesView.as_view()),
+    path('filter/', views.FilterMoviesView.as_view(), name='filter'),
+    path('search/', views.Search.as_view(), name='search'),
+    path('genre/', views.FilterGenre.as_view(), name='genre'),
+    path('slider/', views.Slider.as_view(), name='slider'),
+    path('category/', views.FilterCategory.as_view(), name='category'),
+    path('random/', views.RandomAnime.as_view(), name='random'),
+    path('(?P<id>\d+)/favorite/$', views.favourite_post, name='favourite'),
+    path('favorites/', views.favourite_list, name='favourite_list'),
+    path('accounts/profile/', views.accountSettings, name='profile'),
+    path('last_update/', views.LastUpdate.as_view(), name='last_update'),
+    path("<slug:slug>/", views.MovieDetailView.as_view(), name='anime_main'),
+    path('movies/<slug:slug>', views.CategoryModel.as_view(), name='category_model'),
+    ]
