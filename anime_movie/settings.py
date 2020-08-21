57fb8323fd35a3a1d0589ab5cb4029153bb737b5
harniy <forbedlam@gmail.com>
2020-08-21 13:06:11 +0300

    font-size

--- anime_movie/settings.py
+++ anime_movie/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!!@@c^*i!f40l)7(x7!fo$17pj%hav#2p_ij&bh6r6t=ln-^vu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
